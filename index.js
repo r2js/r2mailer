4673f575b9ec72cdc190be57efd7ce91b79b8a84
Selcuk Fatih Sevinc <airsfs@gmail.com>
2017-03-31 16:01:25 +0300

    v1.0.1
    * config fix

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const mailer = require('nodemailer');
 const pool = require('nodemailer-smtp-pool');
+const log = require('debug')('r2:mailer');
 
 module.exports = function Mailer(app, conf) {
   const mailerConf = conf || app.config('mailer');
   if (!mailerConf) {
-    return Promise.reject('mailer config not found!');
+    return log('mailer config not found!');
   }
 
   return mailer.createTransport(pool(mailerConf));
